e5d2086d868e612dedf0886492e395d0a7723980
Restyled.io <commits@restyled.io>
2024-06-09 01:16:04 +0000

    Restyled by pyment

--- sneakers_ml/models/clip.py
+++ sneakers_ml/models/clip.py
@@ -98,6 +98,7 @@ class CLIPSimilaritySearchTrainer(SimilaritySearchTrainer):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
+        :param data: Sequence[Any]: 
 
         """
         images = data[0].to(self.device)["pixel_values"].squeeze(1)
@@ -209,6 +210,8 @@ class CLIPTextToImageSimilaritySearch(SimilaritySearchPredictor):
         :param text_query: Union[Sequence[str]:
         :param str: Default value = None)
         :param text_query: Union[Sequence[str]:
+        :param str: Default value = None)
+        :param text_query: Union[Sequence[str]: 
         :param str]:  (Default value = None)
 
         """
@@ -313,6 +316,8 @@ class CLIPTextToImageSimilaritySearch(SimilaritySearchPredictor):
         :param top_k: int:
         :param text_query: str:  (Default value = None)
         :param top_k: int:
+        :param text_query: str:  (Default value = None)
+        :param top_k: int: 
         :param text_query: str:  (Default value = None)
 
         """
--- sneakers_ml/models/resnet152_similarity_search.py
+++ sneakers_ml/models/resnet152_similarity_search.py
@@ -72,6 +72,7 @@ class Identity(nn.Module):
         :param x: torch.Tensor:
         :param x: torch.Tensor:
         :param x: torch.Tensor:
+        :param x: torch.Tensor: 
 
         """
         return x
@@ -164,6 +165,7 @@ class ResNet152SimilaritySearchTrainer(SimilaritySearchTrainer):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
+        :param data: Sequence[Any]: 
 
         """
         x = data[0].to(self.device)
@@ -182,6 +184,7 @@ class ResNet152SimilaritySearch(SimilaritySearchPredictor):
     def get_features(self, images: Sequence[Image.Image] = None) -> np.ndarray:
         """
 
+        :param images: Sequence[Image.Image]:  (Default value = None)
         :param images: Sequence[Image.Image]:  (Default value = None)
         :param images: Sequence[Image.Image]:  (Default value = None)
         :param images: Sequence[Image.Image]:  (Default value = None)
@@ -333,6 +336,8 @@ class ResNet152SimilaritySearch(SimilaritySearchPredictor):
         :param top_k: int:
         :param image: Image.Image:  (Default value = None)
         :param top_k: int:
+        :param image: Image.Image:  (Default value = None)
+        :param top_k: int: 
         :param image: Image.Image:  (Default value = None)
 
         """
