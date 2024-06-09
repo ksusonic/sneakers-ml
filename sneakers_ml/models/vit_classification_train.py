c54d804c1f55cf3ad1de45b50f97d694592da144
Restyled.io <commits@restyled.io>
2024-06-09 00:22:20 +0000

    Restyled by pyment

--- sneakers_ml/models/vit_classification_train.py
+++ sneakers_ml/models/vit_classification_train.py
@@ -91,7 +91,9 @@ class ViTClassificationTrainer:
         :param example_batch: dict[str:
         :param torch: Tensor]:
         :param example_batch: dict[str:
-        :param torch.Tensor]:
+        :param torch: Tensor]:
+        :param example_batch: dict[str: 
+        :param torch.Tensor]: 
 
         """
         inputs = self.processor(list(example_batch["image"]), return_tensors="pt")
@@ -104,7 +106,9 @@ class ViTClassificationTrainer:
         :param batch: Sequence[dict[str:
         :param torch: Tensor]]:
         :param batch: Sequence[dict[str:
-        :param torch.Tensor]]:
+        :param torch: Tensor]]:
+        :param batch: Sequence[dict[str: 
+        :param torch.Tensor]]: 
 
         """
         return {
@@ -117,6 +121,7 @@ class ViTClassificationTrainer:
 
         :param p: transformers.trainer_utils.EvalPrediction:
         :param p: transformers.trainer_utils.EvalPrediction:
+        :param p: transformers.trainer_utils.EvalPrediction: 
 
         """
         predictions = np.argmax(p.predictions, axis=1)
