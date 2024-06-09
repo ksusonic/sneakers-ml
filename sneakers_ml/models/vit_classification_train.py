0622cda1468779967005e6fc221e30233b6e1bd7
Restyled.io <commits@restyled.io>
2024-06-09 01:08:06 +0000

    Restyled by whitespace

--- sneakers_ml/models/vit_classification_train.py
+++ sneakers_ml/models/vit_classification_train.py
@@ -174,8 +174,8 @@ class ViTClassificationTrainer:
         :param torch: Tensor]:
         :param example_batch: dict[str:
         :param torch: Tensor]:
-        :param example_batch: dict[str: 
-        :param torch.Tensor]: 
+        :param example_batch: dict[str:
+        :param torch.Tensor]:
 
         """
         inputs = self.processor(list(example_batch["image"]), return_tensors="pt")
@@ -267,8 +267,8 @@ class ViTClassificationTrainer:
         :param torch: Tensor]]:
         :param batch: Sequence[dict[str:
         :param torch: Tensor]]:
-        :param batch: Sequence[dict[str: 
-        :param torch.Tensor]]: 
+        :param batch: Sequence[dict[str:
+        :param torch.Tensor]]:
 
         """
         return {
@@ -320,7 +320,7 @@ class ViTClassificationTrainer:
         :param p: transformers.trainer_utils.EvalPrediction:
         :param p: transformers.trainer_utils.EvalPrediction:
         :param p: transformers.trainer_utils.EvalPrediction:
-        :param p: transformers.trainer_utils.EvalPrediction: 
+        :param p: transformers.trainer_utils.EvalPrediction:
 
         """
         predictions = np.argmax(p.predictions, axis=1)
