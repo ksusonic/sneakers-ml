314c1f65449f11141f1b0cf98a019232e7d324a2
Restyled.io <commits@restyled.io>
2024-06-09 02:06:16 +0000

    Restyled by pyment

--- sneakers_ml/models/similarity_search_base.py
+++ sneakers_ml/models/similarity_search_base.py
@@ -384,12 +384,16 @@ class SimilaritySearchBase(ABC):  # noqa: B024
         :param numpy_features: np.ndarray:
         :param classes: np.ndarray:
         :param class_to_idx: dict[str:
-        :param int:
-        :param path: str:
+        :param int: param path: str:
         :param numpy_features: np.ndarray:
         :param classes: np.ndarray:
         :param class_to_idx: dict[str:
-        :param int]:
+        :param int: 
+        :param path: str: 
+        :param numpy_features: np.ndarray: 
+        :param classes: np.ndarray: 
+        :param class_to_idx: dict[str: 
+        :param int]: 
 
         """
         save_path = Path(path)
@@ -494,6 +498,7 @@ class SimilaritySearchBase(ABC):  # noqa: B024
         :param path: str:
         :param path: str:
         :param path: str:
+        :param path: str: 
 
         """
         with Path(path).open("rb") as file:
@@ -618,6 +623,7 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param top_k: int:
         :param top_k: int:
         :param top_k: int:
+        :param top_k: int: 
 
         """
         raise NotImplementedError
@@ -716,6 +722,7 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param metadata_path: str:
         :param metadata_path: str:
         :param metadata_path: str:
+        :param metadata_path: str: 
 
         """
         df = pd.read_csv(metadata_path)
@@ -918,6 +925,8 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param top_k: int:
         :param feature: np.ndarray:
         :param top_k: int:
+        :param feature: np.ndarray: 
+        :param top_k: int: 
 
         """
         similarity_matrix = cosine_similarity(self.numpy_features, feature).flatten()
@@ -1068,6 +1077,7 @@ class SimilaritySearchTrainer(SimilaritySearchBase):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
+        :param data: Sequence[Any]: 
 
         """
         raise NotImplementedError
