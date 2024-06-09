d82c56893fe9676c486eb596ba7516e426f76155
Restyled.io <commits@restyled.io>
2024-06-09 01:41:49 +0000

    Restyled by whitespace

--- sneakers_ml/models/clip.py
+++ sneakers_ml/models/clip.py
@@ -127,7 +127,7 @@ class CLIPSimilaritySearchTrainer(SimilaritySearchTrainer):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
-        :param data: Sequence[Any]: 
+        :param data: Sequence[Any]:
 
         """
         images = data[0].to(self.device)["pixel_values"].squeeze(1)
@@ -286,7 +286,7 @@ class CLIPTextToImageSimilaritySearch(SimilaritySearchPredictor):
         :param str: Default value = None)
         :param text_query: Union[Sequence[str]:
         :param str: Default value = None)
-        :param text_query: Union[Sequence[str]: 
+        :param text_query: Union[Sequence[str]:
         :param str]:  (Default value = None)
 
         """
@@ -438,7 +438,7 @@ class CLIPTextToImageSimilaritySearch(SimilaritySearchPredictor):
         :param text_query: str:  (Default value = None)
         :param top_k: int:
         :param text_query: str:  (Default value = None)
-        :param top_k: int: 
+        :param top_k: int:
         :param text_query: str:  (Default value = None)
 
         """
--- sneakers_ml/models/vit_classification_train.py
+++ sneakers_ml/models/vit_classification_train.py
@@ -234,8 +234,8 @@ class ViTClassificationTrainer:
         :param torch: Tensor]:
         :param example_batch: dict[str:
         :param torch: Tensor]:
-        :param example_batch: dict[str: 
-        :param torch.Tensor]: 
+        :param example_batch: dict[str:
+        :param torch.Tensor]:
 
         """
         inputs = self.processor(list(example_batch["image"]), return_tensors="pt")
@@ -387,8 +387,8 @@ class ViTClassificationTrainer:
         :param torch: Tensor]]:
         :param batch: Sequence[dict[str:
         :param torch: Tensor]]:
-        :param batch: Sequence[dict[str: 
-        :param torch.Tensor]]: 
+        :param batch: Sequence[dict[str:
+        :param torch.Tensor]]:
 
         """
         return {
@@ -470,7 +470,7 @@ class ViTClassificationTrainer:
         :param p: transformers.trainer_utils.EvalPrediction:
         :param p: transformers.trainer_utils.EvalPrediction:
         :param p: transformers.trainer_utils.EvalPrediction:
-        :param p: transformers.trainer_utils.EvalPrediction: 
+        :param p: transformers.trainer_utils.EvalPrediction:
 
         """
         predictions = np.argmax(p.predictions, axis=1)
