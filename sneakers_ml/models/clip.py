d49dfdad7a82df1a8ab5401009f6bb8af0042e32
Restyled.io <commits@restyled.io>
2024-06-09 01:04:39 +0000

    Restyled by pyment

--- sneakers_ml/models/clip.py
+++ sneakers_ml/models/clip.py
@@ -88,6 +88,7 @@ class CLIPSimilaritySearchTrainer(SimilaritySearchTrainer):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
+        :param data: Sequence[Any]: 
 
         """
         images = data[0].to(self.device)["pixel_values"].squeeze(1)
@@ -179,6 +180,8 @@ class CLIPTextToImageSimilaritySearch(SimilaritySearchPredictor):
         :param text_query: Union[Sequence[str]:
         :param str: Default value = None)
         :param text_query: Union[Sequence[str]:
+        :param str: Default value = None)
+        :param text_query: Union[Sequence[str]: 
         :param str]:  (Default value = None)
 
         """
@@ -263,6 +266,8 @@ class CLIPTextToImageSimilaritySearch(SimilaritySearchPredictor):
         :param top_k: int:
         :param text_query: str:  (Default value = None)
         :param top_k: int:
+        :param text_query: str:  (Default value = None)
+        :param top_k: int: 
         :param text_query: str:  (Default value = None)
 
         """
--- sneakers_ml/models/similarity_search_base.py
+++ sneakers_ml/models/similarity_search_base.py
@@ -172,12 +172,16 @@ class SimilaritySearchBase(ABC):  # noqa: B024
         :param numpy_features: np.ndarray:
         :param classes: np.ndarray:
         :param class_to_idx: dict[str:
-        :param int:
-        :param path: str:
+        :param int: param path: str:
         :param numpy_features: np.ndarray:
         :param classes: np.ndarray:
         :param class_to_idx: dict[str:
-        :param int]:
+        :param int: 
+        :param path: str: 
+        :param numpy_features: np.ndarray: 
+        :param classes: np.ndarray: 
+        :param class_to_idx: dict[str: 
+        :param int]: 
 
         """
         save_path = Path(path)
@@ -229,6 +233,7 @@ class SimilaritySearchBase(ABC):  # noqa: B024
         :param path: str:
         :param path: str:
         :param path: str:
+        :param path: str: 
 
         """
         with Path(path).open("rb") as file:
@@ -300,6 +305,7 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param top_k: int:
         :param top_k: int:
         :param top_k: int:
+        :param top_k: int: 
 
         """
         raise NotImplementedError
@@ -345,6 +351,7 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param metadata_path: str:
         :param metadata_path: str:
         :param metadata_path: str:
+        :param metadata_path: str: 
 
         """
         df = pd.read_csv(metadata_path)
@@ -441,6 +448,8 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param top_k: int:
         :param feature: np.ndarray:
         :param top_k: int:
+        :param feature: np.ndarray: 
+        :param top_k: int: 
 
         """
         similarity_matrix = cosine_similarity(self.numpy_features, feature).flatten()
@@ -538,6 +547,7 @@ class SimilaritySearchTrainer(SimilaritySearchBase):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
+        :param data: Sequence[Any]: 
 
         """
         raise NotImplementedError
