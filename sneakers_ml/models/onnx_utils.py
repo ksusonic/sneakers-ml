9ca558f7ce4dd89722e792079e10033c11ecfc21
Restyled.io <commits@restyled.io>
2024-06-09 01:46:19 +0000

    Restyled by pyment

--- sneakers_ml/models/onnx_utils.py
+++ sneakers_ml/models/onnx_utils.py
@@ -87,6 +87,7 @@ def get_device(device: str) -> str:
     :param device: str:
     :param device: str:
     :param device: str:
+    :param device: str: 
 
     """
     if device.lower().startswith("cuda"):
@@ -97,6 +98,7 @@ def get_device(device: str) -> str:
 def get_providers(device: str = "cpu") -> list[str]:
     """
 
+    :param device: str:  (Default value = "cpu")
     :param device: str:  (Default value = "cpu")
     :param device: str:  (Default value = "cpu")
     :param device: str:  (Default value = "cpu")
@@ -329,6 +331,8 @@ def get_session(model_path: str, device: str = "cpu") -> rt.InferenceSession:
     :param model_path: str:
     :param device: str:  (Default value = "cpu")
     :param model_path: str:
+    :param device: str:  (Default value = "cpu")
+    :param model_path: str: 
     :param device: str:  (Default value = "cpu")
 
     """
@@ -565,6 +569,9 @@ def save_torch_model(model: torch.nn.Module, torch_input_tensor: torch.Tensor, m
     :param model: torch.nn.Module:
     :param torch_input_tensor: torch.Tensor:
     :param model_path: str:
+    :param model: torch.nn.Module: 
+    :param torch_input_tensor: torch.Tensor: 
+    :param model_path: str: 
 
     """
     save_path = Path(model_path)
@@ -807,6 +814,9 @@ def save_clip_model(model: torch.nn.Module, torch_input_tensors: tuple[torch.Ten
     :param model: torch.nn.Module:
     :param torch_input_tensors: tuple[torch.Tensor]:
     :param model_path: str:
+    :param model: torch.nn.Module: 
+    :param torch_input_tensors: tuple[torch.Tensor]: 
+    :param model_path: str: 
 
     """
     save_path = Path(model_path)
@@ -1055,6 +1065,9 @@ def save_sklearn_model(model: BaseEstimator, x: np.ndarray, path: str) -> None:
     :param model: BaseEstimator:
     :param x: np.ndarray:
     :param path: str:
+    :param model: BaseEstimator: 
+    :param x: np.ndarray: 
+    :param path: str: 
 
     """
     onx = to_onnx(model, x[:1].astype(np.float32))
@@ -1216,8 +1229,10 @@ def save_catboost_model(model: Union[CatBoostRegressor, CatBoostClassifier], pat
     :param model: Union[CatBoostRegressor:
     :param CatBoostClassifier: param path: str:
     :param model: Union[CatBoostRegressor:
-    :param CatBoostClassifier]:
-    :param path: str:
+    :param CatBoostClassifier: param path: str:
+    :param model: Union[CatBoostRegressor: 
+    :param CatBoostClassifier]: 
+    :param path: str: 
 
     """
     save_path = Path(path)
@@ -1613,11 +1628,16 @@ def save_model(
     :param torch: Tensor]:
     :param path: str:
     :param model: Union[BaseEstimator:
-    :param torch.nn.Module:
-    :param CatBoostClassifier]:
-    :param x: Union[np.ndarray:
-    :param torch.Tensor]:
+    :param torch: nn.Module:
+    :param CatBoostClassifier: param x: Union[np.ndarray:
+    :param torch: Tensor]:
     :param path: str:
+    :param model: Union[BaseEstimator: 
+    :param torch.nn.Module: 
+    :param CatBoostClassifier]: 
+    :param x: Union[np.ndarray: 
+    :param torch.Tensor]: 
+    :param path: str: 
 
     """
     if isinstance(model, torch.nn.Module):
@@ -1782,7 +1802,9 @@ def format_inputs(x: Union[np.ndarray, torch.Tensor]) -> np.ndarray:
     :param x: Union[np.ndarray:
     :param torch: Tensor]:
     :param x: Union[np.ndarray:
-    :param torch.Tensor]:
+    :param torch: Tensor]:
+    :param x: Union[np.ndarray: 
+    :param torch.Tensor]: 
 
     """
     if isinstance(x, torch.Tensor):
@@ -2021,7 +2043,10 @@ def predict(onnx_session: rt.InferenceSession, x: Union[np.ndarray, torch.Tensor
     :param torch: Tensor]:
     :param onnx_session: rt.InferenceSession:
     :param x: Union[np.ndarray:
-    :param torch.Tensor]:
+    :param torch: Tensor]:
+    :param onnx_session: rt.InferenceSession: 
+    :param x: Union[np.ndarray: 
+    :param torch.Tensor]: 
 
     """
     input_name = onnx_session.get_inputs()[0].name
@@ -2258,7 +2283,10 @@ def predict_clip(onnx_session: rt.InferenceSession, x: dict[str, np.array]) -> n
     :param np: array]:
     :param onnx_session: rt.InferenceSession:
     :param x: dict[str:
-    :param np.array]:
+    :param np: array]:
+    :param onnx_session: rt.InferenceSession: 
+    :param x: dict[str: 
+    :param np.array]: 
 
     """
     input_name_1 = onnx_session.get_inputs()[0].name
--- sneakers_ml/models/resnet152_similarity_search.py
+++ sneakers_ml/models/resnet152_similarity_search.py
@@ -99,6 +99,7 @@ class Identity(nn.Module):
         :param x: torch.Tensor:
         :param x: torch.Tensor:
         :param x: torch.Tensor:
+        :param x: torch.Tensor: 
 
         """
         return x
@@ -218,6 +219,7 @@ class ResNet152SimilaritySearchTrainer(SimilaritySearchTrainer):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
+        :param data: Sequence[Any]: 
 
         """
         x = data[0].to(self.device)
@@ -236,6 +238,7 @@ class ResNet152SimilaritySearch(SimilaritySearchPredictor):
     def get_features(self, images: Sequence[Image.Image] = None) -> np.ndarray:
         """
 
+        :param images: Sequence[Image.Image]:  (Default value = None)
         :param images: Sequence[Image.Image]:  (Default value = None)
         :param images: Sequence[Image.Image]:  (Default value = None)
         :param images: Sequence[Image.Image]:  (Default value = None)
@@ -468,6 +471,8 @@ class ResNet152SimilaritySearch(SimilaritySearchPredictor):
         :param top_k: int:
         :param image: Image.Image:  (Default value = None)
         :param top_k: int:
+        :param image: Image.Image:  (Default value = None)
+        :param top_k: int: 
         :param image: Image.Image:  (Default value = None)
 
         """
