0f8aaacd7a0f002205b48f52477020edbdd6da8e
Restyled.io <commits@restyled.io>
2024-06-09 01:15:00 +0000

    Restyled by whitespace

--- sneakers_ml/models/resnet152_similarity_search.py
+++ sneakers_ml/models/resnet152_similarity_search.py
@@ -75,7 +75,7 @@ class Identity(nn.Module):
         :param x: torch.Tensor:
         :param x: torch.Tensor:
         :param x: torch.Tensor:
-        :param x: torch.Tensor: 
+        :param x: torch.Tensor:
 
         """
         return x
@@ -167,7 +167,7 @@ class ResNet152SimilaritySearchTrainer(SimilaritySearchTrainer):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
-        :param data: Sequence[Any]: 
+        :param data: Sequence[Any]:
 
         """
         x = data[0].to(self.device)
@@ -336,7 +336,7 @@ class ResNet152SimilaritySearch(SimilaritySearchPredictor):
         :param image: Image.Image:  (Default value = None)
         :param top_k: int:
         :param image: Image.Image:  (Default value = None)
-        :param top_k: int: 
+        :param top_k: int:
         :param image: Image.Image:  (Default value = None)
 
         """
