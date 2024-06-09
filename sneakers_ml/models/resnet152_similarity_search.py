6540a8677ff6257b9b9d5d140b5f5b3032dda83f
Restyled.io <commits@restyled.io>
2024-06-09 01:10:16 +0000

    Restyled by pyment

--- sneakers_ml/models/resnet152_similarity_search.py
+++ sneakers_ml/models/resnet152_similarity_search.py
@@ -67,6 +67,7 @@ class Identity(nn.Module):
         :param x: torch.Tensor:
         :param x: torch.Tensor:
         :param x: torch.Tensor:
+        :param x: torch.Tensor: 
 
         """
         return x
@@ -154,6 +155,7 @@ class ResNet152SimilaritySearchTrainer(SimilaritySearchTrainer):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
+        :param data: Sequence[Any]: 
 
         """
         x = data[0].to(self.device)
@@ -172,6 +174,7 @@ class ResNet152SimilaritySearch(SimilaritySearchPredictor):
     def get_features(self, images: Sequence[Image.Image] = None) -> np.ndarray:
         """
 
+        :param images: Sequence[Image.Image]:  (Default value = None)
         :param images: Sequence[Image.Image]:  (Default value = None)
         :param images: Sequence[Image.Image]:  (Default value = None)
         :param images: Sequence[Image.Image]:  (Default value = None)
@@ -308,6 +311,8 @@ class ResNet152SimilaritySearch(SimilaritySearchPredictor):
         :param top_k: int:
         :param image: Image.Image:  (Default value = None)
         :param top_k: int:
+        :param image: Image.Image:  (Default value = None)
+        :param top_k: int: 
         :param image: Image.Image:  (Default value = None)
 
         """
--- sneakers_ml/models/vit_classification_train.py
+++ sneakers_ml/models/vit_classification_train.py
@@ -173,7 +173,9 @@ class ViTClassificationTrainer:
         :param example_batch: dict[str:
         :param torch: Tensor]:
         :param example_batch: dict[str:
-        :param torch.Tensor]:
+        :param torch: Tensor]:
+        :param example_batch: dict[str: 
+        :param torch.Tensor]: 
 
         """
         inputs = self.processor(list(example_batch["image"]), return_tensors="pt")
@@ -268,7 +270,9 @@ class ViTClassificationTrainer:
         :param batch: Sequence[dict[str:
         :param torch: Tensor]]:
         :param batch: Sequence[dict[str:
-        :param torch.Tensor]]:
+        :param torch: Tensor]]:
+        :param batch: Sequence[dict[str: 
+        :param torch.Tensor]]: 
 
         """
         return {
@@ -322,6 +326,7 @@ class ViTClassificationTrainer:
         :param p: transformers.trainer_utils.EvalPrediction:
         :param p: transformers.trainer_utils.EvalPrediction:
         :param p: transformers.trainer_utils.EvalPrediction:
+        :param p: transformers.trainer_utils.EvalPrediction: 
 
         """
         predictions = np.argmax(p.predictions, axis=1)
