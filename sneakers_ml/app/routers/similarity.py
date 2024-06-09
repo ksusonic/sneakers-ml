c8d299e74ebe60fbc44066733c43495ac43c7cba
Daniil <ksusonic@yandex.ru>
2024-06-09 19:24:06 +0300

    Add similarity search to bot (#229)

--- sneakers_ml/app/routers/similarity.py
+++ sneakers_ml/app/routers/similarity.py
@@ -1,46 +1,50 @@
 import os
 import time
+from hashlib import md5
 
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
 from hydra import compose, initialize
 from loguru import logger
+from PIL import Image
 
 from sneakers_ml.app.config import config
-from sneakers_ml.app.models.image import Request, Response
+from sneakers_ml.app.models.image import Response
 from sneakers_ml.app.service.redis import RedisCache
-from sneakers_ml.models.clip import CLIPTextToImageSimilaritySearch
+from sneakers_ml.models.resnet152_similarity_search import ResNet152SimilaritySearch
 
-searcher: CLIPTextToImageSimilaritySearch = None
+searcher: ResNet152SimilaritySearch = None
 redis = RedisCache(host=config.redis_host, port=config.redis_port)
 
-router: APIRouter = APIRouter(prefix="/text-to-image", tags=["text-to-image"])
+router: APIRouter = APIRouter(prefix="/similarity-search", tags=["similarity-search"])
 
 
 @router.on_event("startup")
 async def load_model():
     configs_rel_path = os.path.relpath(config.ml_config_path, start=os.path.dirname(os.path.abspath(__file__)))
     with initialize(version_base=None, config_path=str(configs_rel_path), job_name="fastapi"):
-        cfg = compose(config_name="cfg_text_to_image")
+        cfg = compose(config_name="cfg_similarity_search")
         global searcher
-        searcher = CLIPTextToImageSimilaritySearch(
-            cfg.embeddings_path, cfg.model_path, cfg.metadata_path, cfg.base_model
-        )
-    logger.info("Loaded CLIPTextToImageSimilaritySearch")
+        searcher = ResNet152SimilaritySearch(cfg.embeddings_path, cfg.model_path, cfg.metadata_path)
+    logger.info("Loaded ResNet152SimilaritySearch")
 
 
-@router.post("/clip/")
-async def post_text_to_image(request: Request) -> Response:
-    cached_prediction = redis.get(request.text)
-    if cached_prediction:
-        logger.info("Found cached prediction for image: {}", request.text)
-        return cached_prediction
+@router.post("/upload/")
+async def post_similarity_search(image: UploadFile) -> Response:
+    image.file.seek(0)
+    image = Image.open(image.file)
+
+    image_key = md5(image.tobytes()).hexdigest()  # nosec B324 (weak hash)
+    cached_search = redis.get(image_key)
+    if cached_search:
+        logger.info("Found cached search for image: {}", image_key)
+        return cached_search
 
     start_time = time.time()
-    predictions = searcher.predict(top_k=3, text_query=request.text)
+    predictions = searcher.predict(3, image)
     end_time = time.time()
     logger.info("Predicted {} in {} seconds", predictions, end_time - start_time)
 
-    redis.set(request.text, predictions, ttl=3600)
+    redis.set(image_key, predictions, ttl=3600)
 
     metadata = list(
         map(
