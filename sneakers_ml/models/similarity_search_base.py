759746b7f6aef29cb3f3004f297c072d80d85443
Restyled.io <commits@restyled.io>
2024-06-09 01:58:38 +0000

    Restyled by whitespace

--- sneakers_ml/models/similarity_search_base.py
+++ sneakers_ml/models/similarity_search_base.py
@@ -366,12 +366,12 @@ class SimilaritySearchBase(ABC):  # noqa: B024
         :param numpy_features: np.ndarray:
         :param classes: np.ndarray:
         :param class_to_idx: dict[str:
-        :param int: 
-        :param path: str: 
-        :param numpy_features: np.ndarray: 
-        :param classes: np.ndarray: 
-        :param class_to_idx: dict[str: 
-        :param int]: 
+        :param int:
+        :param path: str:
+        :param numpy_features: np.ndarray:
+        :param classes: np.ndarray:
+        :param class_to_idx: dict[str:
+        :param int]:
 
         """
         save_path = Path(path)
@@ -470,7 +470,7 @@ class SimilaritySearchBase(ABC):  # noqa: B024
         :param path: str:
         :param path: str:
         :param path: str:
-        :param path: str: 
+        :param path: str:
 
         """
         with Path(path).open("rb") as file:
@@ -589,7 +589,7 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param top_k: int:
         :param top_k: int:
         :param top_k: int:
-        :param top_k: int: 
+        :param top_k: int:
 
         """
         raise NotImplementedError
@@ -682,7 +682,7 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param metadata_path: str:
         :param metadata_path: str:
         :param metadata_path: str:
-        :param metadata_path: str: 
+        :param metadata_path: str:
 
         """
         df = pd.read_csv(metadata_path)
@@ -873,8 +873,8 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param top_k: int:
         :param feature: np.ndarray:
         :param top_k: int:
-        :param feature: np.ndarray: 
-        :param top_k: int: 
+        :param feature: np.ndarray:
+        :param top_k: int:
 
         """
         similarity_matrix = cosine_similarity(self.numpy_features, feature).flatten()
@@ -1019,7 +1019,7 @@ class SimilaritySearchTrainer(SimilaritySearchBase):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
-        :param data: Sequence[Any]: 
+        :param data: Sequence[Any]:
 
         """
         raise NotImplementedError
