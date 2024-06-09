d7de20e85369d04cf524d5b87dcd99343a69f3d8
Restyled.io <commits@restyled.io>
2024-06-09 00:54:46 +0000

    Restyled by whitespace

--- sneakers_ml/models/clip.py
+++ sneakers_ml/models/clip.py
@@ -85,7 +85,7 @@ class CLIPSimilaritySearchTrainer(SimilaritySearchTrainer):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
-        :param data: Sequence[Any]: 
+        :param data: Sequence[Any]:
 
         """
         images = data[0].to(self.device)["pixel_values"].squeeze(1)
@@ -160,7 +160,7 @@ class CLIPTextToImageSimilaritySearch(SimilaritySearchPredictor):
         :param str: Default value = None)
         :param text_query: Union[Sequence[str]:
         :param str: Default value = None)
-        :param text_query: Union[Sequence[str]: 
+        :param text_query: Union[Sequence[str]:
         :param str]:  (Default value = None)
 
         """
@@ -228,7 +228,7 @@ class CLIPTextToImageSimilaritySearch(SimilaritySearchPredictor):
         :param text_query: str:  (Default value = None)
         :param top_k: int:
         :param text_query: str:  (Default value = None)
-        :param top_k: int: 
+        :param top_k: int:
         :param text_query: str:  (Default value = None)
 
         """
--- sneakers_ml/models/resnet152_similarity_search.py
+++ sneakers_ml/models/resnet152_similarity_search.py
@@ -57,7 +57,7 @@ class Identity(nn.Module):
         :param x: torch.Tensor:
         :param x: torch.Tensor:
         :param x: torch.Tensor:
-        :param x: torch.Tensor: 
+        :param x: torch.Tensor:
 
         """
         return x
@@ -131,7 +131,7 @@ class ResNet152SimilaritySearchTrainer(SimilaritySearchTrainer):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
-        :param data: Sequence[Any]: 
+        :param data: Sequence[Any]:
 
         """
         x = data[0].to(self.device)
@@ -246,7 +246,7 @@ class ResNet152SimilaritySearch(SimilaritySearchPredictor):
         :param image: Image.Image:  (Default value = None)
         :param top_k: int:
         :param image: Image.Image:  (Default value = None)
-        :param top_k: int: 
+        :param top_k: int:
         :param image: Image.Image:  (Default value = None)
 
         """
