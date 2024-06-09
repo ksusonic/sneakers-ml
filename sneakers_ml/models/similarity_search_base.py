1fd903ae048340e0c64e8aa9aa1dec458ad1e3e9
Restyled.io <commits@restyled.io>
2024-06-09 01:09:11 +0000

    Restyled by pyment

--- sneakers_ml/models/similarity_search_base.py
+++ sneakers_ml/models/similarity_search_base.py
@@ -188,12 +188,16 @@ class SimilaritySearchBase(ABC):  # noqa: B024
         :param numpy_features: np.ndarray:
         :param classes: np.ndarray:
         :param class_to_idx: dict[str:
-        :param int:
-        :param path: str:
+        :param int: param path: str:
         :param numpy_features: np.ndarray:
         :param classes: np.ndarray:
         :param class_to_idx: dict[str:
-        :param int]:
+        :param int: 
+        :param path: str: 
+        :param numpy_features: np.ndarray: 
+        :param classes: np.ndarray: 
+        :param class_to_idx: dict[str: 
+        :param int]: 
 
         """
         save_path = Path(path)
@@ -249,6 +253,7 @@ class SimilaritySearchBase(ABC):  # noqa: B024
         :param path: str:
         :param path: str:
         :param path: str:
+        :param path: str: 
 
         """
         with Path(path).open("rb") as file:
@@ -324,6 +329,7 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param top_k: int:
         :param top_k: int:
         :param top_k: int:
+        :param top_k: int: 
 
         """
         raise NotImplementedError
@@ -373,6 +379,7 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param metadata_path: str:
         :param metadata_path: str:
         :param metadata_path: str:
+        :param metadata_path: str: 
 
         """
         df = pd.read_csv(metadata_path)
@@ -477,6 +484,8 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param top_k: int:
         :param feature: np.ndarray:
         :param top_k: int:
+        :param feature: np.ndarray: 
+        :param top_k: int: 
 
         """
         similarity_matrix = cosine_similarity(self.numpy_features, feature).flatten()
@@ -578,6 +587,7 @@ class SimilaritySearchTrainer(SimilaritySearchBase):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
+        :param data: Sequence[Any]: 
 
         """
         raise NotImplementedError
--- sneakers_ml/models/vit_classification_train.py
+++ sneakers_ml/models/vit_classification_train.py
@@ -171,7 +171,9 @@ class ViTClassificationTrainer:
         :param example_batch: dict[str:
         :param torch: Tensor]:
         :param example_batch: dict[str:
-        :param torch.Tensor]:
+        :param torch: Tensor]:
+        :param example_batch: dict[str: 
+        :param torch.Tensor]: 
 
         """
         inputs = self.processor(list(example_batch["image"]), return_tensors="pt")
@@ -264,7 +266,9 @@ class ViTClassificationTrainer:
         :param batch: Sequence[dict[str:
         :param torch: Tensor]]:
         :param batch: Sequence[dict[str:
-        :param torch.Tensor]]:
+        :param torch: Tensor]]:
+        :param batch: Sequence[dict[str: 
+        :param torch.Tensor]]: 
 
         """
         return {
@@ -317,6 +321,7 @@ class ViTClassificationTrainer:
         :param p: transformers.trainer_utils.EvalPrediction:
         :param p: transformers.trainer_utils.EvalPrediction:
         :param p: transformers.trainer_utils.EvalPrediction:
+        :param p: transformers.trainer_utils.EvalPrediction: 
 
         """
         predictions = np.argmax(p.predictions, axis=1)
