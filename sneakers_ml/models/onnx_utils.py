36029fb944091448f9bf4f7a0fbfd316f5971d6a
Restyled.io <commits@restyled.io>
2024-06-09 00:34:05 +0000

    Restyled by whitespace

--- sneakers_ml/models/onnx_utils.py
+++ sneakers_ml/models/onnx_utils.py
@@ -25,7 +25,7 @@ def get_device(device: str) -> str:
     :param device: str:
     :param device: str:
     :param device: str:
-    :param device: str: 
+    :param device: str:
 
     """
     if device.lower().startswith("cuda"):
@@ -81,7 +81,7 @@ def get_session(model_path: str, device: str = "cpu") -> rt.InferenceSession:
     :param device: str:  (Default value = "cpu")
     :param model_path: str:
     :param device: str:  (Default value = "cpu")
-    :param model_path: str: 
+    :param model_path: str:
     :param device: str:  (Default value = "cpu")
 
     """
@@ -129,9 +129,9 @@ def save_torch_model(model: torch.nn.Module, torch_input_tensor: torch.Tensor, m
     :param model: torch.nn.Module:
     :param torch_input_tensor: torch.Tensor:
     :param model_path: str:
-    :param model: torch.nn.Module: 
-    :param torch_input_tensor: torch.Tensor: 
-    :param model_path: str: 
+    :param model: torch.nn.Module:
+    :param torch_input_tensor: torch.Tensor:
+    :param model_path: str:
 
     """
     save_path = Path(model_path)
@@ -185,9 +185,9 @@ def save_clip_model(model: torch.nn.Module, torch_input_tensors: tuple[torch.Ten
     :param model: torch.nn.Module:
     :param torch_input_tensors: tuple[torch.Tensor]:
     :param model_path: str:
-    :param model: torch.nn.Module: 
-    :param torch_input_tensors: tuple[torch.Tensor]: 
-    :param model_path: str: 
+    :param model: torch.nn.Module:
+    :param torch_input_tensors: tuple[torch.Tensor]:
+    :param model_path: str:
 
     """
     save_path = Path(model_path)
@@ -247,9 +247,9 @@ def save_sklearn_model(model: BaseEstimator, x: np.ndarray, path: str) -> None:
     :param model: BaseEstimator:
     :param x: np.ndarray:
     :param path: str:
-    :param model: BaseEstimator: 
-    :param x: np.ndarray: 
-    :param path: str: 
+    :param model: BaseEstimator:
+    :param x: np.ndarray:
+    :param path: str:
 
     """
     onx = to_onnx(model, x[:1].astype(np.float32))
@@ -286,9 +286,9 @@ def save_catboost_model(model: Union[CatBoostRegressor, CatBoostClassifier], pat
     :param CatBoostClassifier: param path: str:
     :param model: Union[CatBoostRegressor:
     :param CatBoostClassifier: param path: str:
-    :param model: Union[CatBoostRegressor: 
-    :param CatBoostClassifier]: 
-    :param path: str: 
+    :param model: Union[CatBoostRegressor:
+    :param CatBoostClassifier]:
+    :param path: str:
 
     """
     save_path = Path(path)
@@ -373,12 +373,12 @@ def save_model(
     :param CatBoostClassifier: param x: Union[np.ndarray:
     :param torch: Tensor]:
     :param path: str:
-    :param model: Union[BaseEstimator: 
-    :param torch.nn.Module: 
-    :param CatBoostClassifier]: 
-    :param x: Union[np.ndarray: 
-    :param torch.Tensor]: 
-    :param path: str: 
+    :param model: Union[BaseEstimator:
+    :param torch.nn.Module:
+    :param CatBoostClassifier]:
+    :param x: Union[np.ndarray:
+    :param torch.Tensor]:
+    :param path: str:
 
     """
     if isinstance(model, torch.nn.Module):
@@ -418,8 +418,8 @@ def format_inputs(x: Union[np.ndarray, torch.Tensor]) -> np.ndarray:
     :param torch: Tensor]:
     :param x: Union[np.ndarray:
     :param torch: Tensor]:
-    :param x: Union[np.ndarray: 
-    :param torch.Tensor]: 
+    :param x: Union[np.ndarray:
+    :param torch.Tensor]:
 
     """
     if isinstance(x, torch.Tensor):
@@ -470,9 +470,9 @@ def predict(onnx_session: rt.InferenceSession, x: Union[np.ndarray, torch.Tensor
     :param onnx_session: rt.InferenceSession:
     :param x: Union[np.ndarray:
     :param torch: Tensor]:
-    :param onnx_session: rt.InferenceSession: 
-    :param x: Union[np.ndarray: 
-    :param torch.Tensor]: 
+    :param onnx_session: rt.InferenceSession:
+    :param x: Union[np.ndarray:
+    :param torch.Tensor]:
 
     """
     input_name = onnx_session.get_inputs()[0].name
@@ -521,9 +521,9 @@ def predict_clip(onnx_session: rt.InferenceSession, x: dict[str, np.array]) -> n
     :param onnx_session: rt.InferenceSession:
     :param x: dict[str:
     :param np: array]:
-    :param onnx_session: rt.InferenceSession: 
-    :param x: dict[str: 
-    :param np.array]: 
+    :param onnx_session: rt.InferenceSession:
+    :param x: dict[str:
+    :param np.array]:
 
     """
     input_name_1 = onnx_session.get_inputs()[0].name
--- sneakers_ml/models/similarity_search_base.py
+++ sneakers_ml/models/similarity_search_base.py
@@ -74,12 +74,12 @@ class SimilaritySearchBase(ABC):  # noqa: B024
         :param numpy_features: np.ndarray:
         :param classes: np.ndarray:
         :param class_to_idx: dict[str:
-        :param int: 
-        :param path: str: 
-        :param numpy_features: np.ndarray: 
-        :param classes: np.ndarray: 
-        :param class_to_idx: dict[str: 
-        :param int]: 
+        :param int:
+        :param path: str:
+        :param numpy_features: np.ndarray:
+        :param classes: np.ndarray:
+        :param class_to_idx: dict[str:
+        :param int]:
 
         """
         save_path = Path(path)
@@ -105,7 +105,7 @@ class SimilaritySearchBase(ABC):  # noqa: B024
         :param path: str:
         :param path: str:
         :param path: str:
-        :param path: str: 
+        :param path: str:
 
         """
         with Path(path).open("rb") as file:
@@ -151,7 +151,7 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param top_k: int:
         :param top_k: int:
         :param top_k: int:
-        :param top_k: int: 
+        :param top_k: int:
 
         """
         raise NotImplementedError
@@ -171,7 +171,7 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param metadata_path: str:
         :param metadata_path: str:
         :param metadata_path: str:
-        :param metadata_path: str: 
+        :param metadata_path: str:
 
         """
         df = pd.read_csv(metadata_path)
@@ -216,8 +216,8 @@ class SimilaritySearchPredictor(SimilaritySearchBase):
         :param top_k: int:
         :param feature: np.ndarray:
         :param top_k: int:
-        :param feature: np.ndarray: 
-        :param top_k: int: 
+        :param feature: np.ndarray:
+        :param top_k: int:
 
         """
         similarity_matrix = cosine_similarity(self.numpy_features, feature).flatten()
@@ -289,7 +289,7 @@ class SimilaritySearchTrainer(SimilaritySearchBase):
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
         :param data: Sequence[Any]:
-        :param data: Sequence[Any]: 
+        :param data: Sequence[Any]:
 
         """
         raise NotImplementedError
