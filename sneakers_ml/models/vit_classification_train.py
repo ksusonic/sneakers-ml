f16794eb8dacaca5f7b4fafc79c31d0d9918dedf
Restyled.io <commits@restyled.io>
2024-06-09 01:30:42 +0000

    Restyled by yapf

--- sneakers_ml/models/vit_classification_train.py
+++ sneakers_ml/models/vit_classification_train.py
@@ -33,11 +33,15 @@ class ViTClassificationTrainer:
 
     def load_dataset(self) -> None:
         """ """
-        self.processor = ViTImageProcessor.from_pretrained(self.cfg.models.vit_transformer.hf_name)
+        self.processor = ViTImageProcessor.from_pretrained(
+            self.cfg.models.vit_transformer.hf_name)
         self.dataset = load_dataset("imagefolder", data_dir=self.cfg.data.path)
-        self.processed_train = self.dataset["train"].with_transform(self.transform)
-        self.processed_val = self.dataset["validation"].with_transform(self.transform)
-        self.processed_test = self.dataset["test"].with_transform(self.transform)
+        self.processed_train = self.dataset["train"].with_transform(
+            self.transform)
+        self.processed_val = self.dataset["validation"].with_transform(
+            self.transform)
+        self.processed_test = self.dataset["test"].with_transform(
+            self.transform)
 
     def set_training_args(self) -> None:
         """ """
@@ -48,8 +52,10 @@ class ViTClassificationTrainer:
 
         self.training_args = TrainingArguments(
             output_dir=self.temp_model_path,
-            per_device_train_batch_size=self.cfg.models.vit_transformer.dataloader.batch_size,
-            per_device_eval_batch_size=self.cfg.models.vit_transformer.dataloader.batch_size,
+            per_device_train_batch_size=self.cfg.models.vit_transformer.
+            dataloader.batch_size,
+            per_device_eval_batch_size=self.cfg.models.vit_transformer.
+            dataloader.batch_size,
             eval_strategy="steps",
             num_train_epochs=self.cfg.models.vit_transformer.num_epochs,
             fp16=True,
@@ -63,8 +69,10 @@ class ViTClassificationTrainer:
             report_to="wandb" if self.cfg.log_wandb else "none",
             load_best_model_at_end=True,
             dataloader_drop_last=False,
-            dataloader_pin_memory=self.cfg.models.vit_transformer.dataloader.pin_memory,
-            dataloader_num_workers=self.cfg.models.vit_transformer.dataloader.num_workers,
+            dataloader_pin_memory=self.cfg.models.vit_transformer.dataloader.
+            pin_memory,
+            dataloader_num_workers=self.cfg.models.vit_transformer.dataloader.
+            num_workers,
         )
 
     def load_model(self) -> None:
@@ -79,17 +87,25 @@ class ViTClassificationTrainer:
             label2id=self.label2id,
         )
 
-        with Path(self.cfg.models.vit_transformer.idx_to_classes).open("wb") as save_file:
-            np.save(save_file, np.array(list(self.label2id.items())), allow_pickle=False)
+        with Path(self.cfg.models.vit_transformer.idx_to_classes).open(
+                "wb") as save_file:
+            np.save(save_file,
+                    np.array(list(self.label2id.items())),
+                    allow_pickle=False)
 
     def save_class_to_idx(self) -> None:
         """ """
         save_path = Path(self.cfg.models.vit_transformer.idx_to_classes)
         save_path.parent.mkdir(parents=True, exist_ok=True)
-        with Path(self.cfg.models.vit_transformer.idx_to_classes).open("wb") as save_file:
-            np.save(save_file, np.array(list(self.label2id.items())), allow_pickle=False)
+        with Path(self.cfg.models.vit_transformer.idx_to_classes).open(
+                "wb") as save_file:
+            np.save(save_file,
+                    np.array(list(self.label2id.items())),
+                    allow_pickle=False)
 
-    def transform(self, example_batch: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
+    def transform(
+            self,
+            example_batch: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
         """
 
         :param example_batch: dict[str:
@@ -218,11 +234,15 @@ class ViTClassificationTrainer:
         :param torch.Tensor]:
 
         """
-        inputs = self.processor(list(example_batch["image"]), return_tensors="pt")
+        inputs = self.processor(list(example_batch["image"]),
+                                return_tensors="pt")
         inputs["labels"] = example_batch["label"]
         return inputs  # type: ignore[no-any-return]
 
-    def collate_fn(self, batch: Sequence[dict[str, torch.Tensor]]) -> dict[str, torch.Tensor]:
+    def collate_fn(
+            self,
+            batch: Sequence[dict[str,
+                                 torch.Tensor]]) -> dict[str, torch.Tensor]:
         """
 
         :param batch: Sequence[dict[str:
@@ -356,7 +376,9 @@ class ViTClassificationTrainer:
             "labels": torch.tensor([x["labels"] for x in batch]),
         }
 
-    def compute_metrics(self, p: transformers.trainer_utils.EvalPrediction) -> dict[str, float]:
+    def compute_metrics(
+            self,
+            p: transformers.trainer_utils.EvalPrediction) -> dict[str, float]:
         """
 
         :param p: transformers.trainer_utils.EvalPrediction:
@@ -426,12 +448,24 @@ class ViTClassificationTrainer:
         predictions = np.argmax(p.predictions, axis=1)
         references = p.label_ids
 
-        f1_macro = self.metric_f1.compute(predictions=predictions, references=references, average="macro")["f1"]
-        f1_micro = self.metric_f1.compute(predictions=predictions, references=references, average="micro")["f1"]
-        f1_weighted = self.metric_f1.compute(predictions=predictions, references=references, average="weighted")["f1"]
-        accuracy = self.metric_accuracy.compute(predictions=predictions, references=references)["accuracy"]
+        f1_macro = self.metric_f1.compute(predictions=predictions,
+                                          references=references,
+                                          average="macro")["f1"]
+        f1_micro = self.metric_f1.compute(predictions=predictions,
+                                          references=references,
+                                          average="micro")["f1"]
+        f1_weighted = self.metric_f1.compute(predictions=predictions,
+                                             references=references,
+                                             average="weighted")["f1"]
+        accuracy = self.metric_accuracy.compute(
+            predictions=predictions, references=references)["accuracy"]
 
-        return {"f1_macro": f1_macro, "f1_micro": f1_micro, "f1_weighted": f1_weighted, "accuracy": accuracy}
+        return {
+            "f1_macro": f1_macro,
+            "f1_micro": f1_micro,
+            "f1_weighted": f1_weighted,
+            "accuracy": accuracy
+        }
 
     def train(self) -> None:
         """ """
@@ -459,21 +493,27 @@ class ViTClassificationTrainer:
 
         logger.info(str(true_metrics))
 
-        loaded_model = ViTForImageClassification.from_pretrained(self.temp_model_path)
+        loaded_model = ViTForImageClassification.from_pretrained(
+            self.temp_model_path)
         loaded_model.eval()
-        torch_input = (torch.randn(1, 3, 224, 224),)
-        save_torch_model(loaded_model, torch_input, self.cfg.models.vit_transformer.onnx_path)
+        torch_input = (torch.randn(1, 3, 224, 224), )
+        save_torch_model(loaded_model, torch_input,
+                         self.cfg.models.vit_transformer.onnx_path)
 
         metrics = list(true_metrics.values())
         for i, metric in enumerate(metrics):
             metrics[i] = round(metric, 2)
         results_save_path = Path(self.cfg.paths.results)
-        with results_save_path.open("a", newline="", encoding="utf-8") as csv_file:
+        with results_save_path.open("a", newline="",
+                                    encoding="utf-8") as csv_file:
             csv_writer = csv.writer(csv_file)
-            csv_writer.writerow([self.cfg.models.vit_transformer.name, *metrics])
+            csv_writer.writerow(
+                [self.cfg.models.vit_transformer.name, *metrics])
 
 
 if __name__ == "__main__":
-    with initialize(version_base=None, config_path="../../config", job_name="vit-train"):
+    with initialize(version_base=None,
+                    config_path="../../config",
+                    job_name="vit-train"):
         cfg_dl = compose(config_name="cfg_dl")
         ViTClassificationTrainer(cfg_dl).train()
