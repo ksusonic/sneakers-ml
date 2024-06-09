2507934b6a5a49c7eff25f0799c533d328fe6106
Restyled.io <commits@restyled.io>
2024-06-09 01:08:07 +0000

    Restyled by yapf

--- sneakers_ml/models/onnx_utils.py
+++ sneakers_ml/models/onnx_utils.py
@@ -109,7 +109,8 @@ def get_providers(device: str = "cpu") -> list[str]:
     :param device: str:  (Default value = "cpu")
 
     """
-    return ["CUDAExecutionProvider", "CPUExecutionProvider"] if device == "cuda" else ["CPUExecutionProvider"]
+    return ["CUDAExecutionProvider", "CPUExecutionProvider"
+            ] if device == "cuda" else ["CPUExecutionProvider"]
 
 
 def get_session(model_path: str, device: str = "cpu") -> rt.InferenceSession:
@@ -206,7 +207,8 @@ def get_session(model_path: str, device: str = "cpu") -> rt.InferenceSession:
     return rt.InferenceSession(model_path, providers=providers)
 
 
-def save_torch_model(model: torch.nn.Module, torch_input_tensor: torch.Tensor, model_path: str) -> None:
+def save_torch_model(model: torch.nn.Module, torch_input_tensor: torch.Tensor,
+                     model_path: str) -> None:
     """
 
     :param model: torch.nn.Module:
@@ -345,11 +347,20 @@ def save_torch_model(model: torch.nn.Module, torch_input_tensor: torch.Tensor, m
         str(model_path),
         input_names=["input"],
         output_names=["output"],
-        dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
+        dynamic_axes={
+            "input": {
+                0: "batch_size"
+            },
+            "output": {
+                0: "batch_size"
+            }
+        },
     )
 
 
-def save_clip_model(model: torch.nn.Module, torch_input_tensors: tuple[torch.Tensor], model_path: str) -> None:
+def save_clip_model(model: torch.nn.Module,
+                    torch_input_tensors: tuple[torch.Tensor],
+                    model_path: str) -> None:
     """
 
     :param model: torch.nn.Module:
@@ -490,9 +501,17 @@ def save_clip_model(model: torch.nn.Module, torch_input_tensors: tuple[torch.Ten
         input_names=["input_ids", "attention_mask"],
         output_names=["text_features"],
         dynamic_axes={
-            "input_ids": {0: "batch_size", 1: "sequence_length"},
-            "attention_mask": {0: "batch_size", 1: "sequence_length"},
-            "text_features": {0: "batch_size"},
+            "input_ids": {
+                0: "batch_size",
+                1: "sequence_length"
+            },
+            "attention_mask": {
+                0: "batch_size",
+                1: "sequence_length"
+            },
+            "text_features": {
+                0: "batch_size"
+            },
         },
         opset_version=13,
     )
@@ -636,7 +655,8 @@ def save_sklearn_model(model: BaseEstimator, x: np.ndarray, path: str) -> None:
         file.write(onx.SerializeToString())
 
 
-def save_catboost_model(model: Union[CatBoostRegressor, CatBoostClassifier], path: str) -> None:
+def save_catboost_model(model: Union[CatBoostRegressor, CatBoostClassifier],
+                        path: str) -> None:
     """
 
     :param model: Union[CatBoostRegressor:
@@ -741,7 +761,8 @@ def save_catboost_model(model: Union[CatBoostRegressor, CatBoostClassifier], pat
 
 
 def save_model(
-    model: Union[BaseEstimator, torch.nn.Module, CatBoostRegressor, CatBoostClassifier],
+    model: Union[BaseEstimator, torch.nn.Module, CatBoostRegressor,
+                 CatBoostClassifier],
     x: Union[np.ndarray, torch.Tensor],
     path: str,
 ) -> None:
@@ -1069,7 +1090,8 @@ def format_inputs(x: Union[np.ndarray, torch.Tensor]) -> np.ndarray:
     raise ValueError(msg)
 
 
-def predict(onnx_session: rt.InferenceSession, x: Union[np.ndarray, torch.Tensor]) -> np.ndarray:
+def predict(onnx_session: rt.InferenceSession,
+            x: Union[np.ndarray, torch.Tensor]) -> np.ndarray:
     """
 
     :param onnx_session: rt.InferenceSession:
@@ -1207,7 +1229,8 @@ def predict(onnx_session: rt.InferenceSession, x: Union[np.ndarray, torch.Tensor
     return onnx_session.run([output_name], {input_name: input_value})[0]
 
 
-def predict_clip(onnx_session: rt.InferenceSession, x: dict[str, np.array]) -> np.ndarray:
+def predict_clip(onnx_session: rt.InferenceSession,
+                 x: dict[str, np.array]) -> np.ndarray:
     """
 
     :param onnx_session: rt.InferenceSession:
